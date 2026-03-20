98db1c36c4ee47fcf05c1241c44379a64fa5d38f
Lakshmi Murugappan <lakshmi.murugappan@tngtech.com>
2026-03-20 16:00:24 +0000

    Combine AppVaccinateFormComponent and AppPatientSessionRecordComponent
    Moves the vaccination form directly into the record component, wrapping  it in a section card with a "Record <programme> vaccination" heading. Reorders the identity confirmation question to appear above the pre-screening checks heading.

--- app/components/app_patient_session_record_component.rb
+++ app/components/app_patient_session_record_component.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 class AppPatientSessionRecordComponent < ViewComponent::Base
-  erb_template <<-ERB
-    <% if policy(vaccination_record).new? %>
-      <h3 class="nhsuk-heading-m"><%= heading %></h3>
-      <%= render AppVaccinateFormComponent.new(vaccinate_form) %>
-    <% end %>
-  ERB
-
   def initialize(patient:, session:, programme:, current_user:, vaccinate_form:)
     @patient = patient
     @session = session
@@ -29,11 +22,122 @@ class AppPatientSessionRecordComponent < ViewComponent::Base
   attr_reader :patient, :session, :current_user, :programme, :vaccinate_form
 
   delegate :policy, to: :helpers
-  delegate :academic_year, to: :session
+  delegate :academic_year, :team, to: :session
 
-  def registration_status
-    @registration_status ||= patient.registration_status(session:)
+  def url
+    session_patient_programme_vaccinations_path(session, patient, programme)
   end
+
+  def vaccination_record
+    VaccinationRecord.new(patient:, session:, programme:)
+  end
+
+  def vaccine_criteria
+    @vaccine_criteria ||= patient.vaccine_criteria(programme:, academic_year:)
+  end
+
+  def vaccine_methods
+    @vaccine_methods ||=
+      begin
+        approved_vaccine_methods = vaccine_criteria.vaccine_methods
+
+        if current_user.is_nurse? || current_user.is_prescriber?
+          return approved_vaccine_methods
+        end
+        return [] unless healthcare_assistant?
+
+        approved_vaccine_methods.select do |vaccine_method|
+          (
+            vaccine_method == "injection" && session.national_protocol_enabled?
+          ) || (vaccine_method == "nasal" && session.pgd_supply_enabled?) ||
+            (
+              session.psd_enabled? &&
+                patient.has_patient_specific_direction?(
+                  academic_year:,
+                  programme_type: programme.type,
+                  team:,
+                  vaccine_method:
+                )
+            )
+        end
+      end
+  end
+
+  def show_supplied_by_user_id_outside_vaccine_method?
+    @show_supplied_by_user_id_outside_vaccine_method ||=
+      healthcare_assistant? &&
+        vaccine_methods.none? do |vaccine_method|
+          has_patient_specific_direction?(vaccine_method:)
+        end
+  end
+
+  def show_supplied_by_user_id_inside_vaccine_method?(vaccine_method)
+    return false if show_supplied_by_user_id_outside_vaccine_method?
+
+    healthcare_assistant? && !has_patient_specific_direction?(vaccine_method:)
+  end
+
+  def has_patient_specific_direction?(vaccine_method:)
+    session.psd_enabled? &&
+      patient.has_patient_specific_direction?(
+        academic_year:,
+        programme_type: programme.type,
+        team:,
+        vaccine_method:
+      )
+  end
+
+  def healthcare_assistant? = current_user.is_healthcare_assistant?
+
+  def dose_sequence
+    patient.programme_status(programme, academic_year:).dose_sequence
+  end
+
+  COMMON_DELIVERY_SITES = {
+    "injection" => %w[left_arm_upper_position right_arm_upper_position],
+    "nasal" => %w[nose]
+  }.freeze
+
+  CommonDeliverySite = Struct.new(:value, :label)
+
+  def common_delivery_site_options(vaccine_method)
+    common_delivery_sites = COMMON_DELIVERY_SITES.fetch(vaccine_method)
+
+    options =
+      common_delivery_sites.map do |value|
+        label = VaccinationRecord.human_enum_name(:delivery_site, value)
+        CommonDeliverySite.new(value:, label:)
+      end
+
+    has_more_delivery_sites =
+      (
+        Vaccine::AVAILABLE_DELIVERY_SITES.fetch(vaccine_method) -
+          common_delivery_sites
+      ).present?
+
+    if has_more_delivery_sites
+      options << CommonDeliverySite.new(value: "other", label: "Other")
+    end
+
+    options
+  end
+
+  def vaccination_name
+    vaccination =
+      if programme.has_multiple_vaccine_methods?
+        Vaccine.human_enum_name(:method, vaccine_methods.first).downcase
+      else
+        "vaccination"
+      end
+
+    "#{programme.name_in_sentence} #{vaccination}"
+  end
+
+  def ask_not_taking_medication? = programme.doubles? || programme.flu?
+
+  def ask_not_pregnant? = programme.td_ipv?
+
+  def ask_asthma_flare_up? = vaccine_methods.include?("nasal")
 
   def can_record_today?
     return true unless session.requires_registration?
@@ -46,10 +150,6 @@ class AppPatientSessionRecordComponent < ViewComponent::Base
       attendance_record.location_id == session.location_id &&
         attendance_record.date == Date.current
     end
-  end
-
-  def vaccination_record
-    VaccinationRecord.new(patient:, session:, programme:)
   end
 
   def default_vaccinate_form
--- spec/components/app_patient_session_record_component_spec.rb
+++ spec/components/app_patient_session_record_component_spec.rb
@@ -7,17 +7,28 @@ describe AppPatientSessionRecordComponent do
     described_class.new(
       patient:,
       session:,
-      programme: programmes.first,
+      programme:,
       current_user:,
-      vaccinate_form: VaccinateForm.new
+      vaccinate_form:
     )
   end
 
-  let(:current_user) { create(:user) }
-  let(:programmes) { [Programme.hpv] }
-  let(:session) { create(:session, :today, programmes:) }
+  let(:team) { create(:team, programmes:) }
+  let(:current_user) { create(:user, team:) }
+  let(:programme) { Programme.hpv }
+  let(:programmes) { [programme] }
+  let(:session) { create(:session, :today, team:, programmes:) }
   let(:patient) do
-    create(:patient, :consent_given_triage_not_needed, :in_attendance, session:)
+    create(
+      :patient,
+      :consent_given_triage_not_needed,
+      :in_attendance,
+      session:,
+      given_name: "Hari"
+    )
+  end
+  let(:vaccinate_form) do
+    VaccinateForm.new(current_user:, patient:, session:, programme:)
   end
 
   describe "#render?" do
@@ -41,7 +52,12 @@ describe AppPatientSessionRecordComponent do
 
     context "patient attended the session yesterday but not marked as attending today" do
       let(:session) do
-        create(:session, programmes:, dates: [Date.yesterday, Date.current])
+        create(
+          :session,
+          team:,
+          programmes:,
+          dates: [Date.yesterday, Date.current]
+        )
       end
 
       let(:patient) do
@@ -73,21 +89,151 @@ describe AppPatientSessionRecordComponent do
       it { should be(false) }
 
       context "but the session was yesterday" do
-        let(:session) { create(:session, :yesterday, programmes:) }
+        let(:session) { create(:session, :yesterday, team:, programmes:) }
 
         it { should be(false) }
       end
     end
 
     context "session requires no registration" do
-      let(:session) { create(:session, :requires_no_registration, programmes:) }
+      let(:session) do
+        create(:session, :requires_no_registration, team:, programmes:)
+      end
 
       it { should be(true) }
 
       context "but the session was yesterday" do
-        let(:session) { create(:session, :yesterday, programmes:) }
+        let(:session) { create(:session, :yesterday, team:, programmes:) }
 
         it { should be(false) }
+      end
+    end
+  end
+
+  describe "#render" do
+    before do
+      stub_authorization(allowed: true)
+      patient.programme_status(
+        programme,
+        academic_year: session.academic_year
+      ).assign
+    end
+
+    it { should have_heading("Record HPV vaccination") }
+    it { should have_content("Has Hari confirmed their identity?") }
+    it { should have_field("No, it was confirmed by somebody else") }
+
+    context "with a flu programme" do
+      let(:programme) { Programme.flu }
+
+      it { should have_text("Is Hari ready for their flu injection?") }
+      it { should have_field("Yes") }
+      it { should have_field("No") }
+      it { should have_field("Left arm (upper position)") }
+      it { should have_field("Right arm (upper position)") }
+      it { should_not have_field("Nose") }
+      it { should have_field("Other") }
+    end
+
+    context "with a flu programme and consent to nasal spray only" do
+      let(:programme) { Programme.flu }
+
+      let(:patient) do
+        create(
+          :patient,
+          :consent_given_nasal_only_triage_not_needed,
+          :in_attendance,
+          session:,
+          given_name: "Hari"
+        )
+      end
+
+      it { should have_text("Is Hari ready for their flu nasal spray?") }
+      it { should have_field("Yes") }
+      it { should have_field("No") }
+      it { should_not have_field("Left arm (upper position)") }
+      it { should_not have_field("Right arm (upper position)") }
+      it { should_not have_field("Nose") }
+      it { should_not have_field("Other") }
+    end
+
+    context "with a flu programme, consent to nasal spray, but triaged for injection" do
+      let(:programme) { Programme.flu }
+
+      let(:patient) do
+        create(
+          :patient,
+          :consent_given_injection_and_nasal_triage_safe_to_vaccinate_injection,
+          :in_attendance,
+          session:,
+          given_name: "Hari"
+        )
+      end
+
+      it { should have_text("Is Hari ready for their flu injection?") }
+      it { should have_field("Left arm (upper position)") }
+      it { should have_field("Right arm (upper position)") }
+      it { should_not have_field("Nose") }
+      it { should have_field("Other") }
+    end
+
+    context "with an HPV programme" do
+      it { should have_text("Is Hari ready for their HPV vaccination?") }
+      it { should have_field("Left arm (upper position)") }
+      it { should have_field("Right arm (upper position)") }
+      it { should_not have_field("Nose") }
+      it { should have_field("Other") }
+    end
+  end
+
+  describe "#dose_sequence" do
+    subject { component.send(:dose_sequence) }
+
+    let(:prior_vaccination_records) { nil }
+
+    before do
+      prior_vaccination_records # create any prior records before assigning status
+      patient.programme_status(
+        programme,
+        academic_year: session.academic_year
+      ).assign
+    end
+
+    context "with HPV programme" do
+      let(:programme) { Programme.hpv }
+
+      it { should eq(1) }
+    end
+
+    context "with Td/IPV programme" do
+      let(:programme) { Programme.td_ipv }
+
+      it { should eq(5) }
+    end
+
+    context "with MenACWY programme" do
+      let(:programme) { Programme.menacwy }
+
+      it { should eq(1) }
+    end
+
+    context "with flu programme" do
+      let(:programme) { Programme.flu }
+
+      it { should eq(1) }
+    end
+
+    context "with MMR programme" do
+      let(:programme) { Programme.mmr }
+
+      it { should eq(1) }
+
+      context "with an existing vaccination record" do
+        let(:prior_vaccination_records) do
+          create(:vaccination_record, patient:, programme:, dose_sequence: 1)
+        end
+
+        it { should eq(2) }
       end
     end
   end
