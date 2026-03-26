1114e74727139a334579751c659262ad4f85da00
Joshua Frost <joshuafrost.work@gmail.com>
2026-03-26 16:12:08 +0000

    Add "Review vaccs history" status * Adds new status that patients get for a programme when they have been triaged as "Safe to vaccinate" but a new vaccination record has appeared in their history.
    Jira-Issue: MAV-5582

--- app/models/patient/programme_status.rb
+++ app/models/patient/programme_status.rb
@@ -68,6 +68,7 @@ class Patient::ProgrammeStatus < ApplicationRecord
     needs_consent
     has_refusal
     needs_triage
+    review_vaccination_history
     due
     cannot_vaccinate
     vaccinated
@@ -89,6 +90,10 @@ class Patient::ProgrammeStatus < ApplicationRecord
   }.freeze
 
   NEEDS_TRIAGE_STATUSES = { "needs_triage" => 30 }.freeze
+
+  REVIEW_VACCINATION_HISTORY_STATUSES = {
+    "review_vaccination_history" => 35
+  }.freeze
 
   DUE_STATUSES = { "due" => 40 }.freeze
 
@@ -112,6 +117,7 @@ class Patient::ProgrammeStatus < ApplicationRecord
          **NEEDS_CONSENT_STATUSES,
          **HAS_REFUSAL_STATUSES,
          **NEEDS_TRIAGE_STATUSES,
+         **REVIEW_VACCINATION_HISTORY_STATUSES,
          **DUE_STATUSES,
          **CANNOT_VACCINATE_STATUSES,
          **VACCINATED_STATUSES
